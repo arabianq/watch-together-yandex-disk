af064dc9dd52a52b52a8c6811593dd1dd3c55417
Alexander Tarasov <a.tevg@ya.ru>
2025-07-20 01:15:53 +0300

    add username to header

--- web/custom_widgets/header.py
+++ web/custom_widgets/header.py
@@ -1,6 +1,6 @@
 from nicegui import ui
 
-from web.misc import logout
+from web.misc import logout, check_user
 
 
 async def draw_header():
@@ -8,4 +8,8 @@ async def draw_header():
         with ui.row(wrap=False).classes("w-full justify-between") as header_row:
             ui.button(icon="home", on_click=lambda: ui.navigate.to("/"))
             ui.button(text="Rooms", icon="movie", on_click=lambda: ui.navigate.to("/rooms"))
-            ui.button(icon="logout", on_click=lambda: logout(redirect=True))
+
+            with ui.row(wrap=False).classes("items-center"):
+                if user := await check_user():
+                    ui.html(f"<b>{user.username}</b>")
+                ui.button(icon="logout", on_click=lambda: logout(redirect=True))
