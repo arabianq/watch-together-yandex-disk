da271ee17b26c696d190be616f110d55320f0a59
Alexander Tarasov <a.tevg@ya.ru>
2025-07-20 01:13:33 +0300

    fix incorrect imports order

--- web/custom_widgets/__init__.py
+++ web/custom_widgets/__init__.py
@@ -1,3 +1,3 @@
-from web.custom_widgets.content_card import ContentCard
 from web.custom_widgets.content_dialog import ContentDialog
+from web.custom_widgets.content_card import ContentCard
 from web.custom_widgets.header import draw_header
